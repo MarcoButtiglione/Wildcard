2371e3e058386b07f4b0ae05a381842509cd225d
Marco Buttiglione <marco.buttiglione98@gmail.com>
2022-11-04 13:41:34 +0100

    Menu prototype

---
+++ game/wildcard/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+    private Level _currentLevel=Level.Menu;
+
+    /*
+    [SerializeField] private GameObject _loaderCanvas;
+    [SerializeField] private Image _progressBar;
+    private float _target;
+    */
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        _currentLevel = 0;
+    }
+
+    
+    private void LoadScene(string sceneName)
+    {
+        /*
+        _target = 0;
+        _progressBar.fillAmount = 0;
+        */
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        
+        //TODO
+        /*
+        scene.allowSceneActivation = false;
+        
+        _loaderCanvas.SetActive(true);
+        do
+        {
+            _target = scene.progress;
+        } while (scene.progress < 0.9f);
+        
+        scene.allowSceneActivation = true;
+        _loaderCanvas.SetActive(false);
+        */
+    }
+
+    /*
+    private void Update()
+    {
+        _progressBar.fillAmount = Mathf.MoveTowards(_progressBar.fillAmount, _target, 3 * Time.deltaTime);
+    }
+    */
+    
+    
+    
+    public void PlayLevel(Level level)
+    {
+        _currentLevel = level;
+
+        switch (level)
+        {
+            case Level.Menu:
+                break;
+            case Level.StorySuzy:
+                break;
+            case Level.StoryTullio:
+                break;
+            case Level.StoryTobia:
+                break;
+            case Level.StoryLaura:
+                break;
+            case Level.StoryBendy:
+                break;
+            case Level.Research1:
+                LoadScene("Research360_Piggy");
+                break;
+            case Level.Research2:
+                break;
+            case Level.Research3:
+                break;
+            case Level.Research4:
+                break;
+            case Level.Research5:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(level), level, null);
+        }
+        
+    }
+    
+    public void PlayMainMenu()
+    {
+        LoadScene("MainMenu");
+        _currentLevel = 0;
+    }
+}
+
+public enum Level
+{
+    Menu,
+    StorySuzy,
+    StoryTullio,
+    StoryTobia,
+    StoryLaura,
+    StoryBendy,
+    Research1,
+    Research2,
+    Research3,
+    Research4,
+    Research5,
+    
+}
+
---
+++ game/wildcard/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _buttonsMenu;
+    [SerializeField] private GameObject _buttonsStory;
+    [SerializeField] private GameObject _buttonsResearch;
+    [SerializeField] private GameObject _buttonsExploration;
+
+    private void Awake()
+    {
+        _buttonsExploration.SetActive(false);
+        _buttonsMenu.SetActive(true);
+        _buttonsResearch.SetActive(false);
+        _buttonsStory.SetActive(false);
+    }
+
+    public void ClickStory()
+    {
+        _buttonsExploration.SetActive(false);
+        _buttonsMenu.SetActive(false);
+        _buttonsResearch.SetActive(false);
+        _buttonsStory.SetActive(true);
+    }
+    public void ClickResearch()
+    {
+        _buttonsExploration.SetActive(false);
+        _buttonsMenu.SetActive(false);
+        _buttonsResearch.SetActive(true);
+        _buttonsStory.SetActive(false);
+    }
+    public void ClickExploration()
+    {
+        _buttonsExploration.SetActive(true);
+        _buttonsMenu.SetActive(false);
+        _buttonsResearch.SetActive(false);
+        _buttonsStory.SetActive(false);
+    }
+    public void ClickReturn()
+    {
+        _buttonsExploration.SetActive(false);
+        _buttonsMenu.SetActive(true);
+        _buttonsResearch.SetActive(false);
+        _buttonsStory.SetActive(false);
+    }
+    
+    public void PlayResearchSuzy()
+    {
+        Debug.Log("BANANA");
+        LevelManager.Instance.PlayLevel(Level.Research1);
+    }
+}
