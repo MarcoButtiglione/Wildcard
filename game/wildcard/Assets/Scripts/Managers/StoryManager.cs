188eb9937434e24c601319001639512997856740
Marco Buttiglione <marco.buttiglione98@gmail.com>
2022-11-07 17:43:24 +0100

    Susy story done

--- game/wildcard/Assets/Scripts/Managers/StoryManager.cs
+++ game/wildcard/Assets/Scripts/Managers/StoryManager.cs
@@ -7,25 +7,41 @@ public class StoryManager : MonoBehaviour
 {
     private int _state;
 
+    private GameObject _camera;
+    private Vector3 _cameraPos;
     [SerializeField] private GameObject[] _gameObject;
     private GameObject _character;
+    [SerializeField] private float _initRot;
     [SerializeField] private float _speed = 0.1f;
     private bool _isMoving;
+    private float _characterProgression;
     
-    [SerializeField] private double _radius=3f;
-    [SerializeField] private float _heigth=1f;
+    [SerializeField] private float _radius=3f;
+    [SerializeField] private float _heigth=3f;
     
     
     private void Awake()
     {
-        _character = GameObject.Find("Character");
-        _character.transform.position = new Vector3(0f, 3f, 0f);
+        _camera = GameObject.Find("XR Origin");
+        _cameraPos = _camera.transform.position;
+        
+        _character = GameObject.Find("CharacterParent");
+        _character.transform.position = new Vector3(_radius+_cameraPos.x , _heigth+_cameraPos.y, _cameraPos.z);
+        _character.transform.rotation = new Quaternion(0, _initRot, 0, 0);
+        
         _isMoving = false;
+        _characterProgression = 0;
         
         _state = 0;
-        for (var i = 0; i < _gameObject.Length; i++)
+        
+        for (int i = 0; i < _gameObject.Length; i++)
         {
-            _gameObject[i]=Instantiate(_gameObject[i], new Vector3(i+1f, 3f,0f), Quaternion.identity);
+            float angle = (i+1) * Mathf.PI*2f / (_gameObject.Length+1);
+            float x =(Mathf.Cos(angle)*_radius)+_cameraPos.x ;
+            float y = _heigth+_cameraPos.y;
+            float z = (Mathf.Sin(angle) * _radius)+_cameraPos.z;
+            Vector3 newPos = new Vector3(x, y, z);
+            _gameObject[i]=Instantiate(_gameObject[i], newPos, Quaternion.identity);
         }
     }
     
@@ -39,9 +55,22 @@ public class StoryManager : MonoBehaviour
 
     private void MoveCharacter()
     {
-        _character.transform.position += new Vector3(_speed*Time.fixedDeltaTime,0f,0f);
+        _characterProgression += _speed * Time.fixedDeltaTime;
+        
+        float angle = _characterProgression * Mathf.PI*2f / (_gameObject.Length+1);
+        float x =(Mathf.Cos(angle)*_radius)+_cameraPos.x ;
+        float y = _heigth+_cameraPos.y;
+        float z = (Mathf.Sin(angle) * _radius)+_cameraPos.z;
+        Vector3 newPos = new Vector3(x, y, z);
+        
+        _character.transform.position = newPos;
+
+        float rotY = _initRot + (_characterProgression * 360 / _gameObject.Length);
+        
+        _character.transform.rotation = new Quaternion(0, rotY, 0, 0);
+        
         //If the position have overpassed the position of next object in the scene...
-        if (_character.transform.position.x>_state+1)
+        if (_characterProgression>_state+1)
         {
             //If the game is finished
             if (_state +1== _gameObject.Length)
