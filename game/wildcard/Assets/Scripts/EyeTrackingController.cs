6dde1e80829448d5bbd1db38836fd581bc09a79a
Marco Buttiglione <marco.buttiglione98@gmail.com>
2022-12-05 16:07:46 +0100

    Update EyeTrackingController.cs

--- game/wildcard/Assets/Scripts/EyeTrackingController.cs
+++ game/wildcard/Assets/Scripts/EyeTrackingController.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 
 public class EyeTrackingController : MonoBehaviour
 {
-    public void GetEyeTracking()
+    public EyeTrackingData GetEyeTracking()
     {
         var eyeTrackingData = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.World);
-        
+        EyeTrackingData o = new EyeTrackingData
+        {
+            isGazeRayValid = eyeTrackingData.GazeRay.IsValid,
+            rayOrigin = Vector3.zero,
+            rayDirection = Vector3.zero,
+            timestamp = eyeTrackingData.Timestamp,
+            isLeftEyeBlinking = eyeTrackingData.IsLeftEyeBlinking,
+            isRightEyeBlinking = eyeTrackingData.IsRightEyeBlinking,
+        };
+
         if(eyeTrackingData.GazeRay.IsValid)
         {
             // The origin of the gaze ray is a 3D point
-            var rayOrigin = eyeTrackingData.GazeRay.Origin;
+            o.rayOrigin = eyeTrackingData.GazeRay.Origin;
 
             // The direction of the gaze ray is a normalized direction vector
-            var rayDirection = eyeTrackingData.GazeRay.Direction;
-            
+            o.rayDirection = eyeTrackingData.GazeRay.Direction;
         }
-        var timestamp = eyeTrackingData.Timestamp;
-            
-        var isLeftEyeBlinking = eyeTrackingData.IsLeftEyeBlinking;
-        var isRightEyeBlinking = eyeTrackingData.IsRightEyeBlinking;
+        return o;
     }
+}
+
+public struct EyeTrackingData
+{
+    public bool isGazeRayValid;
+    public Vector3 rayOrigin;
+    public Vector3 rayDirection;
+    public float timestamp;
+    public bool isLeftEyeBlinking;
+    public bool isRightEyeBlinking;
 }
