cffd8c864406ff7105af4d400c53f7fc5320281c
IvanC23 <crusco.ivan2@gmail.com>
2022-12-05 17:56:50 +0100

    Update GetDataControllerResearch.cs

--- game/wildcard/Assets/GetDataControllerResearch.cs
+++ game/wildcard/Assets/GetDataControllerResearch.cs
@@ -2,15 +2,18 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Collections;
 
 public class GetDataControllerResearch : MonoBehaviour
 {
     //string filePath = @"D:\Data";
     //string delimiter = ",";
     //The future path of the files
+    private float passedTime = 0f;
     string filePath = @"D:\Data\";
 
-    public class DataToCollect{
+    public class DataToCollect
+    {
         String timeFromStart;
         int isFocusing;
         int isClicking;
@@ -21,13 +24,16 @@ public class GetDataControllerResearch : MonoBehaviour
             isFocusing = v2;
             isClicking = v3;
         }
-        public String GetTime(){
+        public String GetTime()
+        {
             return timeFromStart;
         }
-        public int GetFocusing(){
+        public int GetFocusing()
+        {
             return isFocusing;
         }
-        public int GetClicking(){
+        public int GetClicking()
+        {
             return isClicking;
         }
     }
@@ -37,33 +43,50 @@ public class GetDataControllerResearch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        filePath = filePath +"Research Session " + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".csv";
+        passedTime = 0f;
+        filePath = filePath + "Research Session " + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".csv";
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        myDataList.Add(new DataToCollect(DateTime.Now.ToString("mm-ss"),1,0));
-        WriteCSV();
+        passedTime += Time.fixedDeltaTime;
+        if (passedTime > 0.1f)
+        {
+            myDataList.Add(new DataToCollect(DateTime.Now.ToString("mm.ss.ff"), 1, 0));
+            WriteCSV();
+            passedTime = 0;
+        }
+        //StartCoroutine("Wait");
     }
 
-    public void WriteCSV(){
+    public void WriteCSV()
+    {
 
-        if(myDataList.Count>0){
+        if (myDataList.Count > 0)
+        {
             TextWriter tw = new StreamWriter(filePath, false);
             tw.WriteLine("TimeStamp,IsFocusing,IsWatching");
             tw.Close();
 
             tw = new StreamWriter(filePath, true);
 
-            for(int i=0; i<myDataList.Count;i++){
+            for (int i = 0; i < myDataList.Count; i++)
+            {
                 tw.WriteLine(myDataList[i].GetTime() +
-                ","+ myDataList[i].GetFocusing() +
-                ","+ myDataList[i].GetClicking());
+                "," + myDataList[i].GetFocusing() +
+                "," + myDataList[i].GetClicking());
             }
             tw.Close();
 
         }
 
     }
+
+    /*public IEnumerator Wait(){
+        myDataList.Add(new DataToCollect(DateTime.Now.ToString("mm.ss.ff"),1,0));
+        WriteCSV();
+        yield return new WaitForSecondsRealtime(0.1f);
+    }*/
+
 }
